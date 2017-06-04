ef2407afbabdf322dcb7193d3be593b4cd9cda6a
Tomasz Buchta <tomasz.buchta@hotmail.com>
2017-06-04 19:19:49 +0200

    Add Video channel

---
+++ test/channels/video_channel_test.exs
@@ -0,0 +1,14 @@
+defmodule Rumbl.VideoChannelTest do
+  use Rumbl.ChannelCase
+
+  alias Rumbl.VideoChannel
+
+  setup do
+    {:ok, _, socket} =
+      socket("user_id", %{some: :assign})
+      |> subscribe_and_join(VideoChannel, "videos:1")
+
+    {:ok, socket: socket}
+  end
+
+end
