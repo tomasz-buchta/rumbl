fb234900b843347897597725ad9d713e39e82de8
Tomasz Buchta <tomasz.buchta@hotmail.com>
2017-06-17 12:49:57 +0200

    Correct channel auth

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -8,23 +8,19 @@ defmodule Rumbl.UserSocket do
   transport :websocket, Phoenix.Transports.WebSocket
   # transport :longpoll, Phoenix.Transports.LongPoll
 
-  # Socket params are passed from the client and can
-  # be used to verify and authenticate a user. After
-  # verification, you can put default assigns into
-  # the socket that will be set for all channels, ie
-  #
-  #     {:ok, assign(socket, :user_id, verified_user_id)}
-  #
-  # To deny connection, return `:error`.
-  #
-  # See `Phoenix.Token` documentation for examples in
-  # performing token verification on connect.
+  @max_age 2 * 7 * 24 * 60 * 60
   def connect(%{"token" => token}, socket) do
-    case Phoenix.Token.verify(socket, token) do
-      {:error, _} -> :error
-      {:ok, socket} -> {:ok, socket}
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: @max_age) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        :error
     end
   end
+
+  def connect(_params, _socket), do: :error
+  
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
