a62ec4b5c04ab00ec482052a3e5f017afa76d550
Tomasz Buchta <tomasz.buchta@hotmail.com>
2017-08-05 15:05:39 +0200

    Isolate wolfram

--- apps/rumbl/test/test_helper.exs
+++ apps/rumbl/test/test_helper.exs
@@ -1,6 +1,8 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 ExUnit.configure formatters: [ExUnit.CLIFormatter, ExUnitNotifier]
 ExUnit.configure(timeout: :infinity)
+
+Code.require_file "../../info_sys/test/backends/http_client.exs", __DIR__
 ExUnit.start(exclude: [:skip])
 
 Ecto.Adapters.SQL.Sandbox.mode(Rumbl.Repo, :manual)
